f6cbea3ddf39f2507743aa569847ee08106e12a3
Enrique J. Hernández Blasco <ejhernandez@zentyal.com>
2015-02-25 01:00:49 +0100

    Give support to pass the arguments from CLI
    Autocomplete with argcomplete python package is also possible

--- chess_solver.py
+++ chess_solver.py
@@ -1,6 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import argparse
 from copy import deepcopy
 from itertools import permutations
+import re
+
+
+try:
+    import argcomplete
+    __autocomplete = True
+except:
+    __autocomplete = False
 
 
 class ChessBoard(object):
@@ -186,7 +197,47 @@ def solve(n_rows, n_columns, n_kings, n_bishops, n_rooks, n_knights):
         return solutions
 
 
-sols = solve(4, 4, 0, 5, 0, 0)
-for sol in sols:
-        sol.dump()
-print "\nNumber of solutions: {}".format(len(sols))
+def board(string):
+        match = re.match(r'(\d+)x(\d+)', string)
+        if match:
+                return (int(match.group(1)), int(match.group(2)))
+        else:
+                raise argparse.ArgumentTypeError("Board size format is: MxN")
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description="Chess pieces puzzle solver",
+                                         epilog="If any piece is set different from default other defaults are ignored.")
+        parser.add_argument('--board', '-B', type=board,
+                            default='7x7', help="Set the board size using format MxN. Default: 7x7")
+        parser.add_argument('--kings', '-k', type=int,
+                            help="Number of kings in the chess board. Default: 2")
+        parser.add_argument('--bishops', '-b', type=int,
+                            help="Number of bishops in the chess board. Default: 2")
+        parser.add_argument('--rooks', '-r', type=int,
+                            help="Number of rooks in the chess board. Default: 0")
+        parser.add_argument('--knights', '-n', type=int,
+                            help="Number of knights in the chess board. Default: 1")
+
+        if __autocomplete:
+                argcomplete.autocomplete(parser)
+
+        args = parser.parse_args()
+
+        # if all piece arguments are None, then set the defaults
+        pieces_names = ('kings', 'bishops', 'rooks', 'knights')
+        if all([getattr(args, arg_name) is None for arg_name in pieces_names]):
+                args.kings = 2
+                args.bishops = 2
+                args.rooks = 0
+                args.knights = 1
+        else:
+                # Transform None to 0 for non-defined arguments
+                for arg_name in pieces_names:
+                        if getattr(args, arg_name) is None:
+                                setattr(args, arg_name, 0)
+
+        sols = solve(args.board[0], args.board[1], args.kings, args.bishops, args.rooks, args.knights)
+        for sol in sols:
+                sol.dump()
+        print "\nNumber of solutions: {}".format(len(sols))
